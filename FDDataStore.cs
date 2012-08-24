c30fb9e01e3deb5123f69b71aa00633503232b1c
Nathan Wittstock <nate@fardo.gs>
2012-08-23 17:13:40 -0700

    *License text change*

--- FDDataStore.cs
+++ FDDataStore.cs
@@ -1,7 +1,7 @@
 /*
  * Copyright 2012 Far Dog LLC or its affiliates. All Rights Reserved.
  * 
- * Licensed under the GNU Public License, Version 3.0 (the "License").
+ * Licensed under the GNU General Public License, Version 3.0 (the "License").
  * You may not use this file except in compliance with the License.
  * A copy of the License is located at
  * 
